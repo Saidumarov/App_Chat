9665d46b42c45a564f88bc9f0bfb02247d93a115
Saidumarov <saidumarovjafarxon@gmail.com>
2025-12-12 15:20:45 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ import { IntlProvider } from "next-intl";
 import { LanguageProvider } from "./context/language-provider";
 import Chat from "./chat/Chat";
 import ReactQueryProvider from "./context/ReactQueryProvider";
+import { getStoredSessionId } from "./hooks/storeg";
 function App() {
+  const id = getStoredSessionId();
+  const isMobile = /android|iphone|ipad|ipod|opera mini|iemobile|mobile/i.test(
+    navigator.userAgent
+  );
+  if (!id || !isMobile) {
+    return null;
+  }
   return (
     <>
       <ReactQueryProvider>
